ea60f2d6d7b5384a6f9725699117b5b497c70388
Andrew Alteri <andrewsalteri@gmail.com>
2021-01-17 15:20:27 -0600

    Added start buttons and overall css styling.

--- src/App.js
+++ src/App.js
@@ -3,9 +3,15 @@ import './App.css';
 
 function App() {
   return (
-   <div>
-     
-   </div>
+    <>
+    <h1>Welcome to Pac-Mapper!</h1>
+    <div id="wrapper">
+      <div id="game"></div>
+      <div id="score"></div>
+      <button id="start-button">Start Game</button>
+      <button id="map-button">Create Level</button>
+    </div>
+   </>
   );
 }
 
